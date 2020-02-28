49a864096ecdd44472aa95c867a10c2eac0330ab
Armand Anusca <ioan.anuscapopa@student.manchester.ac.uk>
2020-02-28 00:22:56 +0000

    Small refactoring to files from exercise 3 Optimized some imports Moved files to fit the folder structure

--- src/main/java/uk/ac/man/cs/eventlite/services/VenueServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/services/VenueServiceImpl.java
@@ -1,18 +1,8 @@
-package uk.ac.man.cs.eventlite.dao;
+package uk.ac.man.cs.eventlite.services;
 
-import java.io.InputStream;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import uk.ac.man.cs.eventlite.dao.VenueRepository;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Service
