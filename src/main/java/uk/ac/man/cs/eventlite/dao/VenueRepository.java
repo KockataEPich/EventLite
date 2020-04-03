78bd2522724a5042978c3184b4ac32ca1d4c2df4
Adrian Szvoren <adrian.szvoren@student.manchester.ac.uk>
2020-04-03 14:11:09 +0100

    Implemented back-end for venue search

--- src/main/java/uk/ac/man/cs/eventlite/dao/VenueRepository.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueRepository.java
@@ -5,5 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
 public interface VenueRepository extends CrudRepository<Venue, Long>{
+	
+	public Iterable<Venue> findByOrderByName();
+	
+	public Iterable<Venue> findAllByName(String name);
+
+	public Iterable<Venue> findAllByNameContainingIgnoreCaseOrderByName(String name);
 
 }
--- src/main/java/uk/ac/man/cs/eventlite/services/VenueService.java
+++ src/main/java/uk/ac/man/cs/eventlite/services/VenueService.java
@@ -6,7 +6,11 @@ public interface VenueService {
 
 	public long count();
 
-	public Iterable<Venue> findAll();
+    public Iterable<Venue> findAll();
+	
+	public Iterable<Venue> findByName(String name);
+	
+	public Iterable<Venue> findByNameContaining(String name);
 	
 	public Venue save(Venue venue);
 }
